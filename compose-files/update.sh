abc9ec726f205631247057c1455e0d1fbbcf2d94
Aaron Murray <plugins@omv-extras.org>
2024-12-28 12:35:28 -0600

    check for yml and env files
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

--- compose-files/update.sh
+++ compose-files/update.sh
@@ -5,9 +5,16 @@ tmp="tmp.json"
 
 echo "[" > ${list}
 for i in */; do
+  name="${i/\//}"
+  if [ ! -f "${i}${name}.yml" ]; then
+    echo "No .yml file found for ${name}"
+  fi
+  if [ ! -f "${i}${name}.env" ]; then
+    echo "No .env file found for ${name}"
+  fi
   echo "{" >> ${list}
-  desc="$(cat ${i}/readme)"
-  echo "\"name\": \"${i/\//}\"," >> ${list}
+  desc="$(cat ${i}readme)"
+  echo "\"name\": \"${name}\"," >> ${list}
   echo "\"description\": \"${desc}\"" >> ${list}
   echo "}," >> ${list}
 done
